a340a9876742bf4e68ffd552b542a97c6ef7a603
feihu1996x <gsx0312@qq.com>
2018-08-19 07:39:00 +0800

    logger 日志实现

--- config.py
+++ config.py
@@ -8,12 +8,26 @@
 @version: 1.0
 """
 
+import os
+
+# mysql连接配置
 MYSQL_HOST="192.168.0.100"
 MYSQL_PORT="3306"
 MYSQL_USER="root"
 MYSQL_PASSWORD="9f%IFhJ618"
 MYSQL_DATABASE="hpshSystem"
 
+# 开发服务器配置
 WEB_HOST="0.0.0.0"
 WEB_PORT="8080"
+
+# 日志配置
+LOGGER_PATH=os.getcwd()+"/system.log"
+
+# 发送邮件配置
+MAIL_HOST="smtp.sina.com"
+MAIL_FROM="qinxueonline@sina.com"
+MAIL_TOLIST=["gsx0312@qq.com"]
+MAIL_USERNAME="qinxueonline@sina.com"
+MAIL_PASSWORD="admin123"
 
