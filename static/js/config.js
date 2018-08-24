e0d56496d2d54bef0cf3a363777b4df1c74fdd29
feihu1996x <gsx0312@qq.com>
2018-08-25 07:04:04 +0800

    update static/js/config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,9 +1,12 @@
+// 通用接口前缀
+URL_PREFIX="";
+
 // 获取图表数据接口
-GET_DATA_URL="/data/get_data";
+GET_DATA_URL=URL_PREFIX+"/data/get_data";
 
 // 用户登录接口
-USER_LOGIN_URL="/user/login";
+USER_LOGIN_URL=URL_PREFIX+"/user/login";
 
 // 用户注销接口
-USER_LOGOUT_URL="/user/logout";
+USER_LOGOUT_URL=URL_PREFIX+"/user/logout";
 
